b299706db572514a7cc8c9c66ec75519c560e36e
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-07-28 02:22:28 +0700

    refactor: css container for children page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,14 +4,16 @@ import Header from "../header/Header";
 
 const Layout = (props) => {
   return (
-    <div className="container">
+    <>
       <Header />
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
-      <Outlet />
+      <div className="container">
+        <Outlet />
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
