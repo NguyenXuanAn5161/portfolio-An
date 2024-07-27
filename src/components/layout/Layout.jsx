0ae348d2482b2cf7a573d7dc42a9a1e8b020822a
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-07-28 03:52:00 +0700

    fix: footer auto bottom

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import "./layout.scss";
 
 const Layout = (props) => {
   return (
-    <>
+    <div className="wrapper_layout">
       <Header />
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
-      <div className="container">
+      <div className="container c__outlet">
         <Outlet />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
